0f243714bcaadf17c8cc4a1e24514c211aa8b4aa
larissabonadio <50805040+larissabonadio@users.noreply.github.com>
2022-03-27 16:51:19 -0300

    Criação de funções auxiliares para o cálculo das potencias consumidas

--- modelo_matematico_parras.py
+++ modelo_matematico_parras.py
@@ -22,27 +22,27 @@ node2 = 0
 # Função para identificar o tipo de elemento a partir do seu ID
 def tipo_elemento (id_elemento):
     for i, bomba in wn.pumps():
-        if id_elemento == int(bomba.name):
+        if id_elemento == str(bomba.name):
             return 'BOMBA'
 
     for i, no in wn.junctions():
-        if id_elemento == int(no.name):
+        if id_elemento == str(no.name):
             return 'NO'
 
     for i, reservatorio in wn.reservoirs():
-        if id_elemento == int(reservatorio.name):
+        if id_elemento == str(reservatorio.name):
             return 'RESERVATORIO'
             
     for i, tanque in wn.tanks():
-        if id_elemento == int(tanque.name):
+        if id_elemento == str(tanque.name):
             return 'TANQUE'
             
     for i, trecho in wn.pipes():
-        if id_elemento == int(trecho.name):
+        if id_elemento == str(trecho.name):
             return 'TRECHO'
 
     for i, valvula in wn.valves():
-        if id_elemento == int(valvula.name):
+        if id_elemento == str(valvula.name):
             return 'VALVULA'
 
 #   Função que define a altura geométrica
@@ -71,32 +71,51 @@ def fator_atrito_dw(tipo_bomba, tipo_tubulacao):
             return 0.019
         else:
             return 0.046  
+        
+#   Função que a partir de um elemento percorre a rede (em sequência) para encontrar um trecho (para assim pegar as informações de comprimento, diamentro e rugosidade)
+def verifica_ligacao(node1): 
+    node1 = str(node1)
+    if tipo_elemento(node1) == 'TRECHO':
+        return node1
+    # Se for esses tipos de elementos eles só serão o nó de inicio / final
+    if tipo_elemento(node1) == 'RESERVATORIO' or tipo_elemento(node1) == 'TANQUE' or tipo_elemento(node1) == 'NO':
+        for pump, bomba in wn.pumps():
+            if bomba.start_node_name == str(node1):
+                node1 = bomba.end_node_name
+                verifica_ligacao(node1)
+        for pipe, trecho in wn.pipes():
+            if trecho.start_node_name == str(node1):
+                node1 = trecho
+                return node1 
 
-#   Função que verifica a ligação entre os dois componentes e a qual trecho ele corresponde
-def caminho(node1):
-    global node2
-    
-    # Verificar se o node1 está conectado a uma bomba - Reservatório está sempre ligado a uma bomba
+#   Função que retorna o comprimenro da tubulação especificada (em metros)
+def comprimento_tubulacao(id_trecho):
+    for pipe, trecho in wn.pipes():
+        if (str(trecho) == str(id_trecho)):
+            return trecho.length    
+        
+#   Função que retorna o diametro da tubulação especificada (em metros)
+def diametro_tubulacao(id_trecho):
+    for pipe, trecho in wn.pipes():
+        if (str(trecho) == str(id_trecho)):
+            return trecho.diameter
+
+#   Função que retorna a vazão de uma bomba
+def vazao_bomba(node1):
     for pump, bomba in wn.pumps():
-        if bomba.start_node_name == str(node1):
-            print(bomba.start_node_name)
-            node2 = bomba.end_node_name
-            print(node2)
-
-    return (node1, node2)
-
-#   Função que pega o ID do elemento e verifica com quem está conectado
-def comprimento_tubulacao():
-    return 0.0    
-
+        if(bomba.start_node_name == str(node1)):
+            print(bomba)
     
+
     
 #   Função que retorna o valor em decimal do rendimento / eficiência da bomba   
 def rendimento_bomba(id_bomba):
     return wn.options.energy.global_efficiency / 100 
 
 
-# Leitura das variáveis de entrada
+
+#   FUNÇÃO PRINCIPAL
+# Variáveis
 qt_reservatorio = 0
 qt_eta = 0
 g = 9.81
@@ -105,6 +124,9 @@ Hpe = 0
 Fpe = 0
 Npe = 0
 Lpe = 0
+Dpe = 0
+Qpe = 0
+Vpe = 0
 
 #   Informações sobre os reservatórios de nível fixo: 
 for  reservoir, reservatorio in wn.reservoirs():
@@ -116,21 +138,7 @@ for tank, tanque in wn.tanks():
 #    Coloquei 3 pq sei que representa a ETA
     if tanque.name == '6':        
         qt_eta += 1
-        id_eta = tanque
-    
-
-#   Verifica se o reservatório de nível fixo esta ligada a uma bomba - pega o node final 
-#for pumps, bomba in wn.pumps():
-#    if str(bomba.start_node_name) == str(id_reservatorio):
-#       no_reserv_bomba = bomba.end_node_name
-       
-#   Para chegar em outro componente analiso o cano - tenho o nó inicial e procuro qual é ID do componente final
-#for pipes, cano in wn.pipes():
-#    if(str(cano.start_node_name) == str(no_reserv_bomba)):
-#        print('A bomba está conectada com o cano: ', cano)
-    
-# Informações sobre a(s) bomba(s) ligada(s) ao reservatório fixo        
-    
+        id_eta = tanque    
 
 # 1. Restrições de Potência 
 
@@ -138,13 +146,11 @@ for tank, tanque in wn.tanks():
 
 Hpe += altura_geometrica(reservatorio.base_head, id_eta.elevation)
 Fpe += fator_atrito_dw('CAPTACAO', 'NOVA')
-#Lpe = comprimento_tubulacao(1)
-#Dpe += diametro_tubulacao()
+Lpe = comprimento_tubulacao(verifica_ligacao(id_reservatorio))
+Dpe += diametro_tubulacao(verifica_ligacao(id_reservatorio))
 #Vpe += velocidade_escoamento()
-#Qpe += vazao_bomba()
+Qpe = vazao_bomba(id_reservatorio)
 Npe += rendimento_bomba(2)
-node = caminho(1)
-print(node[0], node[1])
 
 #   PN - Potência consumida pela(s) bomba(s) de captação de ponto(s) de subterrâneo(s)
 
@@ -192,7 +198,6 @@ print(node[0], node[1])
 # 3. Somatório de bombas de captação (nc) 
 
 # 4. Somatório do periodo de simulação
-
 
 
 # Resolvendo o modelo com CPLEX
